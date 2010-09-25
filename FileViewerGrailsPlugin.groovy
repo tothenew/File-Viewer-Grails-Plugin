9c7f879da169404e3192173bf1a8342dd74dbf9b
Himanshu Seth <himanshu@intelligrape.com>
2010-09-25 17:10:05 +0530

    Changed required grails version to 1.1 after testing with 1.1

--- FileViewerGrailsPlugin.groovy
+++ FileViewerGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class FileViewerGrailsPlugin {
     // the plugin version
     def version = "0.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.3 > *"
+    def grailsVersion = "1.1 > *"
     // the other plugins this plugin depends on
 	def dependsOn = [:]
 
