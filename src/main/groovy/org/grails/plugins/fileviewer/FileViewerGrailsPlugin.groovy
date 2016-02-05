2c67ac3c7540803bad95acff9c23d9cc74abd9f2
Puneet Behl <puneetbehl@yahoo.com>
2016-02-05 23:10:08 +0530

    Updated developers & organization

--- src/main/groovy/org/grails/plugins/fileviewer/FileViewerGrailsPlugin.groovy
+++ src/main/groovy/org/grails/plugins/fileviewer/FileViewerGrailsPlugin.groovy
@@ -9,9 +9,12 @@ class FileViewerGrailsPlugin extends Plugin {
     def description = 'Provides a user-friendly way for viewing folders and files on the system, for example logs on the server.'
     def documentation = 'http://github.com/IntelliGrape/File-Viewer-Grails-Plugin/wiki'
     def developers = [
-        [name: 'Himanshu Seth', email: 'himanshu@intelligrape.com'],
-        [name: 'Fabien Benichou', email: 'fabien.benichou@gmail.com']
+        [name: 'Himanshu Seth', email: 'himanshu@tothenew.com'],
+        [name: 'Fabien Benichou', email: 'fabien.benichou@gmail.com'],
+        [name: 'Burt Beckwith', email: 'burt@burtbeckwith.com'],
+        [name: 'Puneet Behl', email: 'puneet.behl007@gmail.com']
     ]
+    def organization = [name: 'TO THE NEW Digital', url: 'http://www.tothenew.com/']
     def issueManagement = [url: 'http://github.com/IntelliGrape/File-Viewer-Grails-Plugin/issues']
     def scm = [url: 'http://github.com/IntelliGrape/File-Viewer-Grails-Plugin']
     def profiles = ['web']
